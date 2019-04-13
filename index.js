912ab72f3d5438d806dfcb8acf6a9a223e177986
wuxiaoming <260895778@qq.com>
2019-04-13 23:13:01 +0800

    加入一个服务器访问性质的日志（其他项目可能会用，先测试看看）

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express'), //express 框架 
-    wechat = require('./wechat/wechat');
+const express = require('express'); //express 框架 
+const wechat = require('./wechat/wechat');
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+const rfs = require('rotating-file-stream')
 let config = require('./config');//引入配置文件
 let privateConfig = require('./private.config');//我自己写的私有配置，该文件未放入git内，可以注释这一段，如果将config中的id和Screct,替换为自己的话
 
 const app = express();//实例express框架
+const logDirectory = path.join(__dirname, 'log')
 
-Object.assign(config,privateConfig) //合并对象，可以注释这一段，如果将config中的id和Screct,替换为自己的话
+// 如果不存在log文件夹，那就创建
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+ 
+// 生成一个定时日志
+var accessLogStream = rfs('access.log', {
+  interval: '1M', // rotate daily
+  size:'200M',
+  path: logDirectory,
+  maxSize:'1G'
+})
+ 
+// 中间件中启用
+app.use(morgan('combined', { stream: accessLogStream }))
+ 
+
+Object.assign(config, privateConfig) //合并对象，可以注释这一段，如果将config中的id和Screct,替换为自己的话
 const wechatApp = new wechat(config); //实例wechat 模块
 
 //用于处理所有进入 80 端口 get 的连接请求
